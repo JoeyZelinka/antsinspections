d4bc1758b6a51b1f2f444f5e611a0e01222d5923
Joey Zelinka <cheflife365@gmail.com>
2022-11-12 11:54:52 -0500

    layout

--- src/components/CertsModal/CertsModal.jsx
+++ src/components/CertsModal/CertsModal.jsx
@@ -40,12 +40,14 @@ export default function CertsModal() {
                     <div className="certImg">
                         <Grid fluid>
                             <Row>
-                                <Col xs={64} sm={64} md={24}><a href="https://www.nachi.org/verify?nachiid=NACHI22082510" target="_blank" rel="noreferrer"><img src="http://www.nachi.org/webseals/s-v2-NACHI22082510.gif" width="98" height="102" alt="Certified by the International Association of Certified Home Inspectors" border="0" /></a></Col>
-                            </Row>
+                                <center className="internachis">
+                                <a href="https://www.nachi.org/verify?nachiid=NACHI22082510" target="_blank" rel="noreferrer"><img src="internachi1.png" width="98" height="102" alt="Certified by the International Association of Certified Home Inspectors" border="0" /></a>
+                            
                                
-                            <Row>
-                                <Col xs={64} sm={64} md={24}><a href="https://www.nachi.org/verify?nachiid=NACHI22082510" target="_blank" rel="noreferrer"><img src="http://www.nachi.org/webseals/internachi-honor-guarantee.png?nachiid=NACHI22082510" width="170" height="170" alt="$10,000 Honor Guarantee, Backed by InterNACHI" border="0" /></a></Col>
-                            </Row>
+                            
+                                <a href="https://www.nachi.org/verify?nachiid=NACHI22082510" target="_blank" rel="noreferrer"><img src="internachi2.png" width="98" height="102" alt="$10,000 Honor Guarantee, Backed by InterNACHI" border="0" /></a>
+                                </center>
+                           </Row>
                             <Row>
                             <Col xs={64} sm={64} md={24}><img className="img" src="codeOfEthics.png" alt="" /></Col>
                             <Col xs={64} sm={64} md={24}><img className="img" src="interNachiQR.png" alt="" /></Col>
